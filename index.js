bb02dc3a8a6789aada1c0718948a379b176f2825
daisyjane johnson <daisyjane.johnson@gmail.com>
2020-08-19 23:11:46 -0600

    finsished what I can

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 'use strict';
-// Require server
-const mongoose = require('mongoose');
-const server = require('./lib/server.js');
-
 
 //Require dotenv
 require('dotenv').config();
+const mongoose = require('mongoose');
+// Require server
+const server = require('./lib/server.js');
+
+
+const MONGODB_URI = 'mongodb://localhost:27017/';
+
+const mongooseOptions = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+mongoose.connect(MONGODB_URI, mongooseOptions);
+
 //Call the start method from server
-server.start();
+server.start(3000);
